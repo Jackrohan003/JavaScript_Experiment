b16ced22be481fc976e25e1b65479e01fc55b396
Rohan Prajapati <58733914+Jackrohan003@users.noreply.github.com>
2021-03-03 18:35:38 +0530

    Create Greet.js

---
+++ React/DestructuringPropsAndState/Greet.js
@@ -0,0 +1,18 @@
+import React from 'react'
+
+// function Greet(){
+//     return <h1>Hello Rohan</h1>
+// }
+
+const Greet=(props)=> {
+    const {name,lastName} = props
+return  (
+<div>
+    <h1>Hello {name} {lastName} </h1>
+    {props.children}
+</div>
+
+)
+
+}
+export default Greet
