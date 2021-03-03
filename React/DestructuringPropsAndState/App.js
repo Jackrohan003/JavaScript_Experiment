f874a2f1e9d4cd5c6160ae58727f50f3b92ae4fd
Rohan Prajapati <58733914+Jackrohan003@users.noreply.github.com>
2021-03-03 18:32:07 +0530

    Destructring Props and State

---
+++ React/DestructuringPropsAndState/App.js
@@ -0,0 +1,30 @@
+import logo from './logo.svg';
+import './App.css';
+import  Greet from './Components/Greet'
+import Welcome from './Components/Welcome'
+import Message from './Components/Message'
+import Counter from './Components/Counter';
+
+function App() {
+  return (
+    <div className="App">
+     <Greet name="Varun" lastName="Prajapati" />  {/* Functional Component   */ }
+     <Welcome name="Varun" lastName="Prajapati" /> {/* Class Component   */ }
+{/*  
+    <Counter></Counter>       
+         <Message />      
+         <Greet name="Rohan" >
+           This is Children Props
+         </Greet>
+         <Greet name="Varun" />
+         <Greet name="Siddharth" >
+           <button>Action</button>
+           </Greet>
+         <Welcome name="Varun"/>
+         <Welcome name="Rohan"/>
+          */}
+    </div>
+  );
+}
+
+export default App;
