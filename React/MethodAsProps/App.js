d05287d75c3de1bdf8d52c57f1cf51e6eb554975
Rohan Prajapati <58733914+Jackrohan003@users.noreply.github.com>
2021-03-04 09:25:18 +0530

    Call Parent Method from Child Component

---
+++ React/MethodAsProps/App.js
@@ -0,0 +1,21 @@
+import logo from './logo.svg';
+import './App.css';
+import  Greet from './Components/Greet'
+import Welcome from './Components/Welcome'
+import Message from './Components/Message'
+import Counter from './Components/Counter';
+import FunctionalClick from './Components/FunctionalClick';
+import ClassClick from './Components/ClassClick';
+import EventBind from './Components/EventBind';
+import ParentComponent from './Components/ParentComponent';
+
+function App() {
+  return (
+    <div className="App">
+       <ParentComponent />
+        {/* <EventBind /> */}
+    </div>
+  );
+}
+
+export default App;
