bd78e73b1f86ff45b0e350b80691aed2ffde3f67
Rohan Prajapati <58733914+Jackrohan003@users.noreply.github.com>
2021-03-10 09:30:50 +0530

    Create Stylesheet.js

---
+++ React/CssStyle/Stylesheet.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import './myStyle.css'
+
+function Stylesheet() {
+   
+    return (
+        <div>
+            <h1 className='primary'>Hello</h1>
+        </div>
+    )
+}
+
+export default Stylesheet
